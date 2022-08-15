fd5875cebc25df553c5140bc5968ddcce6931cea
V <vlad.vlad.1234@yandex.ru>
2022-08-15 16:53:24 +0300

    Tree

---
+++ src/Autor.java
@@ -0,0 +1,17 @@
+public class Autor {
+    private String name;
+    private String LastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public Autor(String name, String lastName) {
+        this.name = name;
+        LastName = lastName;
+    }
+}
